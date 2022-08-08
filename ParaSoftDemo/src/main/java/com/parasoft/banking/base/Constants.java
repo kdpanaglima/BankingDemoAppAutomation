a42e41d7abd8c209c1c28d025c411761b4e31270
KevinPanaglima <KevinPanaglima@PC>
2022-08-08 15:01:25 +0800

    [skip ci]

--- ParaSoftDemo/src/main/java/com/parasoft/banking/base/Constants.java
+++ ParaSoftDemo/src/main/java/com/parasoft/banking/base/Constants.java
@@ -4,7 +4,7 @@ package com.parasoft.banking.base;
 public class Constants {
 	
 	//Configuration
-	public static final String browser = System.getProperty("USE_BROWSER");
+	public static final String browser = System.getenv("USE_BROWSER");
 //	public static final String browser = "chrome";
 	public static final String testsiteurl = "https://parabank.parasoft.com/parabank/index.htm";
 	public static long implicitwait=10;
